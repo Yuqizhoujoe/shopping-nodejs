0e024101279ea388275998713c1a22563112b6bd
damnjoejoe <49086087+Yuqizhoujoe@users.noreply.github.com>
2021-11-13 22:45:15 -0600

    Finished MongoDB oh yeah!!!

--- controllers/shop.js
+++ controllers/shop.js
@@ -75,18 +75,15 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .getOrders({ include: ['products'] })
-        // .getOrders()
-        .then(orders => {
-            console.log(orders);
-            res.render('shop/orders', {
-                path: "/orders",
-                pageTitle: 'your Orders',
-                orders: orders
-            });
-        })
-        .catch(err => console.log(err));
+    req.user.getOrders()
+    .then(orders => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
+          });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -111,16 +111,42 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db.collection('orders')
-            .insertOne(this.cart)
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.name,
+                        email: this.email
+                    }
+                };
+                return db.collection('orders').insertOne(order);
+            })
             .then(result => {
-                this.cart = { items: [] };
+                console.log(result);
+                this.cart = {
+                    items: []
+                };
                 return db.collection('cart')
-                    .updateOne(
-                        { _id: new ObjectId(this._id) },
-                        { $set: { cart: { items: [] } } }
-                    );
+                    .updateOne({
+                        _id: new ObjectId(this._id)
+                    }, {
+                        $set: {
+                            cart: {
+                                items: []
+                            }
+                        }
+                    });
             });
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders')
+            .find({
+                'user._id': new ObjectId(this._id)
+            }).toArray();
     }
 }
 
