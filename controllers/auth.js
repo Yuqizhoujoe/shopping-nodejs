f2fe250fdd1e9ae5493834058db50384daf1139e
damnjoejoe <49086087+Yuqizhoujoe@users.noreply.github.com>
2021-12-07 23:05:33 -0600

    password reset logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -113,6 +113,80 @@ exports.getReset = (req, res, next) => {
   });
 }
 
-exports.postRest = (req, res, next) => {
-  
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+
+    const token = buffer.toString('hex');
+    User.findOne({email: req.body.email})
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'No account with that email');
+        return res.redirect('/reset');
+      }
+
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    })
+    .then(result => {
+      return res.redirect(`/reset/${token}`);
+    })
+    .catch(err => console.log(err));
+  })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+  .then(user => {
+    let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/new-password', {
+    path: '/new-password',
+    pageTitle: 'New Passoword',
+    errorMessage: message,
+    userId: user._id.toString(),
+    passwordToken: token
+  });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const token = req.body.passwordToken;
+  const userId = req.body.userId;
+
+  let resetUser;
+
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+  .then(user => {
+    resetUser = user;
+    return bcrypt.hash(newPassword, 12);
+  })
+  .then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = null;
+    resetUser.resetTokenExpiration = null;
+
+    return resetUser.save();
+  })
+  .then(result => {
+    return res.redirect('/login');
+  })
+  .catch(err => console.log(err));
 }
